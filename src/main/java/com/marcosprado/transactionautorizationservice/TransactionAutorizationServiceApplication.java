a6c21ff303b2c2fef3de55f3b8401189f6d1e129
Marcos Prado <marcospradofelix@gmail.com>
2026-01-06 20:30:37 -0300

    initial project

---
+++ src/main/java/com/marcosprado/transactionautorizationservice/TransactionAutorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcosprado.transactionautorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAutorizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionAutorizationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcosprado/transactionautorizationservice/TransactionAutorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosprado.transactionautorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAutorizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
